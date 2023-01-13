ba192281721fcef8ed6846c7b3b209fc5f00a3fb
ricmoccia <riccardo_moccia@yahoo.com>
2023-01-13 14:21:50 +0100

    sistemata parte grafica di checkUser.jsx

--- src/routes/checkUser.jsx
+++ src/routes/checkUser.jsx
@@ -1,7 +1,18 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import {useForm} from "react-hook-form";
-import {useNavigate, Link, useLocation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Divider from '@mui/material/Divider';
+import PersonIcon from '@mui/icons-material/Person';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 
 export default function CheckUser() {
 
@@ -27,18 +38,59 @@ export default function CheckUser() {
     }
 
     return (
-        <div className="App">
-            <h2>
-                E' stato creato l'utente con i seguenti campi
-            </h2>
 
-            <div>{user?.username}</div>
-            <div>{user?.firstname}</div>
-            <div>{user?.lastname}</div>
-            <div>{user?.email}</div>
-            <div>{user?.role_name}</div>
+        <div className="App">
+
+            <h2>Riepilogo informazioni per l'utente {user?.username}</h2>
+
+            <List
+                sx={{
+                    width: '100%',
+                    maxWidth: 360,
+                    bgcolor: 'background.paper',
+                }}
+            >
+
+                <Divider variant="inset" component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <PersonIcon/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="firstname" secondary={user?.firstname}/>
+                </ListItem>
+
+                <Divider variant="inset" component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <PersonIcon/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="lastname" secondary={user?.lastname}/>
+                </ListItem>
+
+                <Divider variant="inset" component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <LocalPostOfficeIcon/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="email"secondary={user?.email} />
+                </ListItem>
+
+                <Divider variant="inset" component="li"/>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <EngineeringIcon/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="role name" secondary={user?.role_name}/>
+                </ListItem>
+            </List>
         </div>
     );
-
-
 }
